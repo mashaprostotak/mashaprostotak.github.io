f0ddc220571e04cf91311efff8c7ff79471e3822
masha <eremeeva.maria0799@gmail.com>
2026-02-14 21:04:59 +0100

    Add gatsby-plugin-github-pages to fix routing and asset paths

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,16 +4,9 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
-const React = require("react")
-
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
-  
-  // Add base tag for pathPrefix support on GitHub Pages
-  setHeadComponents([
-    React.createElement("base", { key: "base", href: "/me-updated/" })
-  ])
 }
